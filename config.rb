0f79a841088806adbb6b28121d656f7c30aa988d
Paul Barton <info@thisispaul.co.uk>
2016-03-07 13:52:43 +0000

    Reorganising the structure of the site

--- config.rb
+++ config.rb
@@ -1,14 +1,13 @@
-require 'bootstrap-sass'
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/app/assets/css"
-sass_dir = "/app/assets/scss"
-images_dir = "/app/assets/img"
-javascripts_dir = "/app/assets/js"
+css_dir = "/public/assets/css"
+sass_dir = "/public/assets/scss"
+images_dir = "/public/assets/img"
+javascripts_dir = "/public/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
