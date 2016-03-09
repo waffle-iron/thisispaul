e7bb8d6961ca7900c764cc91fff1b7ff255f88e8
Paul Barton <info@thisispaul.co.uk>
2016-03-09 20:51:38 +0000

    Adding babel to the project

---
+++ src/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+import TimeGraph from './time';
+
+TimeGraph.init();
---
+++ src/time/index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+export default {
+
+    id: 'time-graph',
+
+    init: function () {
+        var self = this;
+
+        self.element = document.getElementById(self.id);
+
+        console.debug(self.element);
+    }
+
+};
