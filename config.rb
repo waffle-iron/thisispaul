9cd17fac53bcddb8e913163d1c2e135ef4f11403
Paul Barton <info@thisispaul.co.uk>
2016-03-15 15:32:13 +0000

    Updating the style of the header

--- config.rb
+++ config.rb
@@ -4,16 +4,16 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "/public/assets/css"
-sass_dir = "/public/assets/scss"
-images_dir = "/public/assets/img"
-javascripts_dir = "/public/assets/js"
+css_dir = "public/assets/css"
+sass_dir = "public/assets/scss"
+images_dir = "public/assets/img"
+javascripts_dir = "public/assets/js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
