89c591bc1f177133fdd45c9fad2a97472d458d67
Paul Barton <info@thisispaul.co.uk>
2016-03-15 15:06:24 +0000

    Adding example svg creation

--- src/graphs/time-spent/index.js
+++ src/graphs/time-spent/index.js
@@ -8,13 +8,22 @@ module.exports = objectAssign(BaseGraph, {
     // this is the id
     id: 'time-spent',
 
+    // holds all the shapes for this graph
+    shapes: {},
+
     /**
      *
      */
     start: function () {
-        var self = this;
-        
+        var self = this,
+            width = 100;
 
+        self.shapes.time = self.draw().rect(width, 100);
+
+        setInterval(function () {
+            width++;
+            self.shapes.time.width(width);
+        }, 1000);
     }
 
 });
